3a2ee3a2eb428ef6e63b1400c894b0a6bad71697
nikolay0904 <Nikolay28081992@mail.ru>
2019-03-21 23:52:02 +0700

    variant 5

--- 5/Solution.java
+++ 5/Solution.java
@@ -13,8 +13,30 @@ public class Solution {
   
     
 	static String func1(int year) {
-	
-	}
+        String a = null;
+        
+        
+        if ((year > 1918) && (year != 1918) ) {
+            if (((year % 4 == 0)&&(year % 100 != 0)&&(year % 400!=0))||(year % 400==0)) {
+                a = "12.09." + year;
+            } else {
+                a = "13.09."+ year;
+            }
+        }else if ((year < 1918)&&(year !=1918) ) {
+            if (year % 4 == 0)  {
+                a = "12.09."+ year;
+            } else {
+                a = "13.09."+ year;
+            }
+        }
+        
+        if (year==1918)
+            a = "26.09."+ year;
+        
+        return  a;
+    }
+
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -29,5 +51,7 @@ public class Solution {
 
         bufferedReader.close();
         bufferedWriter.close();
+
+        System.out.println(result);
     }
 }
